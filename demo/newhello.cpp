95d19eb0d616a85e55653f0841505fff00852d31
Vishal Sahu <sahuvishal2712da@gmail.com>
2021-06-02 20:37:49 +0530

    newhlo.cpp

--- demo/newhello.cpp
+++ demo/newhello.cpp
@@ -8,7 +8,12 @@ int main()
     string str;
     cout<<"Enter name:-";
     cin>>str;
+    cout<<"Enter your age:-";
+    int age;
+    cin>>age;
     cout<<"Hello World\n";
     cout<<str<<15+20;
+    cout<<endl<<age;
+
     
 }
