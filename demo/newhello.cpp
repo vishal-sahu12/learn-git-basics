a5117ef99f4ead9a3f982673448216a2f75f31fc
Vishal Sahu <sahuvishal2712da@gmail.com>
2021-06-02 20:30:58 +0530

    Second commit

---
+++ demo/newhello.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string str;
+    cout<<"Enter name:-";
+    cin>>str;
+    cout<<"Hello World\n";
+    cout<<str<<15+20;
+    
+}
